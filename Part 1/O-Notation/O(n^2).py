b8643ed526650f0aa60490812441f6d9ec252fb7
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-25 11:02:24 +1100

    Update O(n^2).py

--- Part 1/O-Notation/O(n^2).py
+++ Part 1/O-Notation/O(n^2).py
@@ -1,4 +1,5 @@
-# O(n^2) or O of n squared is when you iterate over an array within another array, in large datasets these are slower
+# O(n^2) or O of n squared aka Quadratic is when you iterate over an array within another array, in large datasets these are slower
+# Adding another for loop+array makes in O(n^3) or O-cubed, far slower again
 
 def Onsquared(array1, array2):
     for i in array1:
