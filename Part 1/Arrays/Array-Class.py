6c176ed3cf408919c3db1594952e0530039228ec
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-25 16:03:17 +1100

    Update Array-Class.py

--- Part 1/Arrays/Array-Class.py
+++ Part 1/Arrays/Array-Class.py
@@ -13,3 +13,17 @@ print(array.index(69))
 # Change element at index 1 and print it
 array[1] = 96
 print(array[1])
+
+# Removes element at array index 0
+array.pop(0)
+print(array)
+
+def removeIndex(index):
+    try:
+        array.pop(index)
+    except IndexError:
+        print('Index out of range')
+removeIndex(2)
+
+array.insert(1, 9000)
+print(array)
