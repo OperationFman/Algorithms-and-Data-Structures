271f4f2fc47e7b64c8f631c619e29ea1d2fbd1b6
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-25 15:42:19 +1100

    Create Array-Class.py

---
+++ Part 1/Arrays/Array-Class.py
@@ -0,0 +1,15 @@
+# Make array, no memory allocation needed
+array = []
+
+# Insert elements to the end
+array.append(1)
+array.append(69)
+array.append(420)
+
+#Show what's in the array and at index 1
+print(array)
+print(array.index(69))
+
+# Change element at index 1 and print it
+array[1] = 96
+print(array[1])
