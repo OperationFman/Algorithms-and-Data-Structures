bb658d5303858dfdd552137521ba7b5bd4ff121f
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-27 19:43:09 +1100

    Update Building-Linked-List.py

--- Part 1/Linked-Lists/Building-Linked-List.py
+++ Part 1/Linked-Lists/Building-Linked-List.py
@@ -1,8 +1,39 @@
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data = None):
         self.data = data
         self.next = None
 
 class LinkedList:
     def __init__(self):
-        pass
+        self.head = Node()
+
+    def addLast(self, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        total = 0
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+            total += 1
+        return total
+
+    def contains(self):
+        elements = []
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+            elements.append(cur)
+        print(elements)
+
+llist = LinkedList()
+
+llist.addLast(1)
+llist.addLast(2)
+llist.addLast(3)
+
+print(llist.contains())
