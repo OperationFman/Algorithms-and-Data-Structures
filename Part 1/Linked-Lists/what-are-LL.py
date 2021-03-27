1b3bac7266c7dae7b732c819e3f66df31846f9a1
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-27 14:26:20 +1100

    typo

--- Part 1/Linked-Lists/what-are-LL.py
+++ Part 1/Linked-Lists/what-are-LL.py
@@ -3,5 +3,4 @@
 # A node contains an array element/item with a reference to the next node
 # if adding/removing the first node (aka head) you can simply add a new node, point the head to it and refer that node to the rest of the LL. To delete, just point head to the second node
 # Similarly with the Tail, just point to the second last 
-# The head to remove/delete/update are O(1)
 # Doing anything between including the tail will be O(n) since the LL could be 'n' length
