03754d316ee1caa8258d2e2a7e80963261a085f0
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-28 18:29:21 +1100

    Update Queues.py

--- Part 1/Queues/Queues.py
+++ Part 1/Queues/Queues.py
@@ -1,10 +1,39 @@
 class Queue:
+    def __init__(self):
+        self.array = [2, 4, 6, 8, 10]
+
+    def view(self):
+        print(self.array)
+
     def reverse(self, queue):
         result = []
         for i in queue[::-1]:
             result.append(i)
         return result
 
+    def dequeue(self, item):
+        self.array.append(item)
+
+    def peek(self):
+        return self.array[-1]
+
+    def isEmpty(self):
+        if self.array:
+            return False
+        return True
+
+    def pointers(self, start, end):
+        selection = self.array[start:end]
+        return selection
+
 queue = Queue()
-list = [2, 4, 6, 8, 10]
-print(queue.reverse(list))
+# print(queue.reverse(list))
+
+queue.dequeue(12)
+queue.view()
+
+print(queue.peek())
+
+print(queue.isEmpty())
+
+print(queue.pointers(1, 4))
