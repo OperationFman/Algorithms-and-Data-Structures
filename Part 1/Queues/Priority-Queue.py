fb7b8ef63a5939c5d00ce3181b10834fdb9b3105
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-30 13:42:46 +1100

    Update Priority-Queue.py

--- Part 1/Queues/Priority-Queue.py
+++ Part 1/Queues/Priority-Queue.py
@@ -6,14 +6,27 @@ class PriQue:
         print(self.priorityArray)
 
     def insert(self, item):
-        self.priorityArray.append(item)
-        self.priorityArray.sort
+        # self.priorityArray.append(item)
+        # self.priorityArray.sort
+        if self.priorityArray == []:
+            self.priorityArray.append(item)
+        else:
+            for i in self.priorityArray:
+                if item <= i:
+                    self.priorityArray.insert(self.priorityArray.index(i), item)
+                    break
+                else:
+                    self.priorityArray.append(item)
+                    break
     
 pq = PriQue()
 
+pq.insert(5)
 pq.insert(1)
-pq.insert(3)
 pq.insert(2)
 pq.insert(9)
 pq.insert(4)
+pq.insert(4)
+pq.insert(0)
+pq.insert(15)
 pq.view()
