45543db1cc3f9d9ce87946aef8655d3338d5424c
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-31 11:47:19 +1100

    Update Binary-Tree.py

--- Part 2/Binary-Trees/Binary-Tree.py
+++ Part 2/Binary-Trees/Binary-Tree.py
@@ -4,10 +4,19 @@ class Node:
         self.left = None
         self.right = None
 
+    def print(self):
+        print(self.data)
+
 class BinaryTree():
-    def __init__(self):
-        self.root = None
+    def __init__(self, item):
+        self.root = Node(item)
+    
+    def add(self, item):
+        root = self.root
+        node = Node(item)
+        if root.left == None or root.right == None:
+            root.right = node
 
 
-tree = BinaryTree()
-
+node = Node(10)
+node.print()
