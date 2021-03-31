18de4469d5701d6dcad4d850a11f1470133a6b79
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-31 11:54:33 +1100

    Update Binary-Tree.py

--- Part 2/Binary-Trees/Binary-Tree.py
+++ Part 2/Binary-Trees/Binary-Tree.py
@@ -5,18 +5,36 @@ class Node:
         self.right = None
 
     def print(self):
-        print(self.data)
+        if self.left:
+            self.left.print()
+        print(self.data),
+        if self.right:
+            self.right.print()
 
-class BinaryTree():
-    def __init__(self, item):
-        self.root = Node(item)
-    
-    def add(self, item):
-        root = self.root
-        node = Node(item)
-        if root.left == None or root.right == None:
-            root.right = node
-
+    def insert(self, item):
+        if self.data:
+            if item < self.data:
+                if self.left is None:
+                    self.left = Node(item)
+                else:
+                    self.left.insert(item)
+            elif item > self.data:
+                if self.right is None:
+                    self.right = Node(item)
+                else:
+                    self.right.insert(item)
+        else:
+            self.data = item
 
 node = Node(10)
+
+node.insert(5)
+node.insert(15)
+node.insert(6)
+node.insert(1)
+node.insert(8)
+node.insert(12)
+node.insert(18)
+node.insert(17)
+
 node.print()
