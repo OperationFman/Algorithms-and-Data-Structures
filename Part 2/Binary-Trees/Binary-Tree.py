5838f33d236340fbfc983aca6282e9cf595d8a0d
Franklin Moon <franklin.m.moon@gmail.com>
2021-03-31 11:32:37 +1100

    Update Binary-Tree.py

--- Part 2/Binary-Trees/Binary-Tree.py
+++ Part 2/Binary-Trees/Binary-Tree.py
@@ -1,2 +1,12 @@
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.left = None
+        self.right = None
+
 class BinaryTree():
     pass
+
+
+tree = BinaryTree()
+
