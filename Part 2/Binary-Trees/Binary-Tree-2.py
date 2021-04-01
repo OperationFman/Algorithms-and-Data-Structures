fd1c96f4cbc1e595d0ca9203f63fa2f8206478f0
Franklin Moon <franklin.m.moon@gmail.com>
2021-04-01 11:19:49 +1100

    Update Binary-Tree-2.py

--- Part 2/Binary-Trees/Binary-Tree-2.py
+++ Part 2/Binary-Trees/Binary-Tree-2.py
@@ -9,6 +9,7 @@ class BinaryTree:
         self.root = None
 
     def insert(self, data):
+        """Runs the root first then a private method makes in easier to recursively repeat"""
         if self.root == None:
             self.root = Node(data)
             print('Root: ' + str(self.root.data))
@@ -19,13 +20,27 @@ class BinaryTree:
         if item < cur_node.data:
             if cur_node.left == None:
                 cur_node.left = Node(item)
+                print("New Left Node: " + str(cur_node.left.data))
             else:
                 return self._insert(item, cur_node.left)
         elif item > cur_node.data:
             if cur_node.right == None:
                 cur_node.right = Node(item)
+                print("New Right Node: " + str(cur_node.right.data))
             else:
                 return self._insert(item, cur_node.right)
+        else: print("Node value already exists")
+
+    def view(self):
+        if self.root != None:
+            return self._view(self.root)
+
+    def _view(self, item):
+        pass
 
 tree = BinaryTree()
+tree.insert(9)
+tree.insert(5)
+tree.insert(11)
+tree.insert(1)
 tree.insert(9)
